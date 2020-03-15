779ac183cf7871cc850255569dc1e180b3c77650
shuai <1129123733@qq.com>
2020-03-15 09:15:13 +0800

    init repo

---
+++ src/main/java/com/Hello/Helloworld.java
@@ -0,0 +1,12 @@
+package com.Hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class Helloworld {
+    public static void main(String[] args) {
+        SpringApplication.run(Helloworld.class, args);
+    }
+}
---
+++ src/test/java/com/Hello/HelloworldTest.java
@@ -0,0 +1,29 @@
+//package com.Hello;
+//
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.test.context.junit4.SpringRunner;
+//
+//
+///**
+// * springboot单元测试
+// */
+//
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//public class HelloworldTest {
+//
+//
+//    @Autowired
+//    Person person;
+//
+//    @Test
+//    public void contextLoads(){
+//        System.out.println(person);
+//    }
+//
+//
+//}
